bba24d6699d7092fb3dba4e2e61746c27ca5aed5
铁米(黄锦初) <huangjinchu@sygame.com>
2018-12-29 21:19:42 +0800

    添加map generator代码

--- Assets/Scripts/Map/MapGenerator.cs
+++ Assets/Scripts/Map/MapGenerator.cs
@@ -39,6 +39,18 @@ public class MapModel
         this.width = _width;
         this.coordinates = new int[_length, _width];
         this.roomCount = _roomCount;
+        //InitCoordinate();
+    }
+
+    private void InitCoordinate()
+    {
+        for (int i = 0; i < this.length; i++)
+        {
+            for (int j = 0; j < this.width; j++)
+            {
+                this.coordinates[i, j] = 0;
+            }
+        }
     }
 };
 
@@ -51,7 +63,7 @@ public class MapGenerator : MonoBehaviour
 
     void Awake()
     {
-        map = new MapModel(50, 50, 7);
+        map = new MapModel(50, 50, 5);
     }
 
     void Start()
@@ -66,7 +78,7 @@ public class MapGenerator : MonoBehaviour
 
     void CreateRoomFlow()   //创建房间流程
     {
-        RoomModel firstRoom = CreateRoom(Vector3.zero, 10, 10);  //在原点创建第一个房间
+        RoomModel firstRoom = CreateRoom(Vector3.zero, 3, 3);  //在原点创建第一个房间
 
         int curRoomCount = 1;
 
@@ -74,10 +86,23 @@ public class MapGenerator : MonoBehaviour
 
         while (curRoomCount < map.RoomCount)
         {
+            int nextLengh = 3;
 
-            FindNextRoomCenter(firstRoom.Center);
+            int nextWidth = 3;
 
-            curRoomCount++;
+            Vector3 nextCenter = FindNextRoomCenter(firstRoom.Center, nextLengh, nextWidth);
+
+            if (CheckSpaceEnough(nextCenter, nextLengh, nextWidth))
+            {
+
+                CreateRoom(nextCenter, nextLengh, nextWidth);
+                curRoomCount++;
+            }
+            else
+            {
+                // nextCenter
+            }
+
         }
     }
     RoomModel CreateRoom(Vector3 _center, int _long, int _width)
@@ -100,18 +125,62 @@ public class MapGenerator : MonoBehaviour
         return room;
     }
 
-    Vector3 FindNextRoomCenter(Vector3 _lastCenter)        //寻找下一个房间的原点
+    void CreatePath()
     {
-        Vector3 retCenter = Vector3.zero;
 
-        int tmpDir = Random.Range();
-
-        return retCenter;
     }
 
-    bool CheckSpaceEnough()
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="_lastCenter">  上一个房间的中心点   </param>
+    /// <param name="_nextLength">  下一个房间的长度    </param>
+    /// <param name="_nextWidth">   下一个房间的宽度    </param>
+    /// <returns></returns>
+    Vector3 FindNextRoomCenter(Vector3 _lastCenter, int _nextLength, int _nextWidth)        //寻找下一个房间的原点
     {
-        return false;
+        Vector3 nextCenter = _lastCenter;
+
+        int tmpDir = Random.Range((int)RoomDir.Left, (int)RoomDir.Bottom + 1);
+
+        Debug.Log("tmpDir " + tmpDir);
+
+        int pathLength = Random.Range(15, 30);   //随机出房间通路的长度，此处先固定，以后可扩展
+
+        Debug.Log("pathLength " + pathLength);
+
+        if (tmpDir == (int)RoomDir.Left)
+        {
+            nextCenter.x -= (pathLength + _nextLength * 0.5f);
+        }
+        else if (tmpDir == (int)RoomDir.Right)
+        {
+            nextCenter.x += (pathLength - _nextLength * 0.5f);
+        }
+        else if (tmpDir == (int)RoomDir.Top)
+        {
+            nextCenter.y += (pathLength + _nextWidth * 0.5f);
+        }
+        else if (tmpDir == (int)RoomDir.Bottom)
+        {
+            nextCenter.y += (pathLength - _nextWidth * 0.5f);
+        }
+
+        return nextCenter;
+    }
+
+    bool CheckSpaceEnough(Vector3 _nextCenter, int _nextLengh, int _nextWidth)
+    {
+
+        if (Physics.CheckBox(_nextCenter, new Vector3(_nextLengh, 10, _nextWidth)))
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+
     }
 
 }
